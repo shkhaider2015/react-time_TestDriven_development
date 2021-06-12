13cfe3be050b10afcf244d92d1cf46d4aea879f0
Shakeel Haider <43218437+shkhaider2015@users.noreply.github.com>
2021-06-12 19:24:44 +0500

    kjkkj

--- src/components/App/Timer/Timer.jsx
+++ src/components/App/Timer/Timer.jsx
@@ -1,11 +1,32 @@
 import React from 'react'
 import "./Timer.css";
+import TimerButton from "../../TimerButton/TimerButton";
 
 
-const Timer = () => 
-{
+const Timer = () => {
+
+    const [minutes, setMinutes] = React.useState(25);
+    const [seconds, setSeconds] = React.useState(0);
+    const [isOn, setIsOn] = React.useState(false);
+
+    const startTimer = () => {
+        console.log('Starting timer.');
+    }
+    const stopTimer = () => {
+        console.log('Stop timer.');
+    }
+    const resetTimer = () => {
+        console.log('Reset timer.');
+    }
+
+
     return <div className="timer-container" >
-
+        <div className="time-display"></div>
+        <div className="timer-button-container">
+            <TimerButton buttonAction={startTimer} buttonValue={'Start'} />
+            <TimerButton buttonAction={stopTimer} buttonValue={'Stop'} />
+            <TimerButton buttonAction={resetTimer} buttonValue={'Reset'} />
+        </div>
     </div>
 }
 
--- src/components/App/Timer/Timer.spec.js
+++ src/components/App/Timer/Timer.spec.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { shallow } from "enzyme"
+import { shallow, mount } from "enzyme"
 import Timer from "./Timer";
 
 describe(
@@ -17,5 +17,40 @@ describe(
                 expect(container.find("div").length).toBeGreaterThanOrEqual(1)
             }
         )
+
+        it("should render instances of the TimerButton component", () => {
+            expect(container.find("TimerButton").length).toEqual(3)
+          })
     }
 )
+
+
+describe('mounted Timer', () => {
+    let container;
+  
+    beforeEach(() => (container = mount(<Timer />)));
+  
+    it('invokes startTimer when the start button is clicked', () => {
+      const spy = jest.spyOn(container.instance(), 'startTimer');
+      container.instance().forceUpdate();
+      expect(spy).toHaveBeenCalledTimes(0);
+      container.find('.start-timer').first().simulate('click');
+      expect(spy).toHaveBeenCalledTimes(1);
+    });
+  
+    it('invokes stopTimer when the stop button is clicked', () => {
+      const spy = jest.spyOn(container.instance(), 'stopTimer');
+      container.instance().forceUpdate();
+      expect(spy).toHaveBeenCalledTimes(0);
+      container.find('.stop-timer').first().simulate('click');
+      expect(spy).toHaveBeenCalledTimes(1);
+    });
+  
+    it('invokes resetTimer when the reset button is clicked', () => {
+      const spy = jest.spyOn(container.instance(), 'resetTimer');
+      container.instance().forceUpdate();
+      expect(spy).toHaveBeenCalledTimes(0);
+      container.find('.reset-timer').first().simulate('click');
+      expect(spy).toHaveBeenCalledTimes(1);
+    });
+  });
